1f62d150fd7d5c62b40445854da881454843bc74
JawadHamdan03 <eng.jawad121@hotmail.com>
2025-05-20 19:32:03 +0300

    Contains Duplicate

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿
+int[] nums = { 1, 2, 4, 3 };
+Console.WriteLine(ContainsDuplicate(nums));
+
+
+bool ContainsDuplicate(int[] nums)
+{
+    // track every element you have seen then check 
+    HashSet<int> set = new HashSet<int>();
+
+    for (global::System.Int32 i = 0; i < nums.Length; i++)
+    {
+        if (set.Contains(nums[i]))
+        {
+            return true;
+        }
+        set.Add(nums[i]);
+    }
+
+    return false;
+}
